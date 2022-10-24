2e6b12a9669634e808c6f789b59ec1d3a6d71678
Fulbabu <fulbabucse@gmail.com>
2022-10-24 21:08:13 +0600

    project initialize and folder structure

---
+++ src/pages/UserActivities/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>This is Log in pages</h1>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/sharedPages/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>This is Navbar pages</h1>
+    </div>
+  );
+};
+
+export default Navbar;
