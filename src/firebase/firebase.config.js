4e2dbd6b44f77bcaae3deea272c57eeac67d1806
Fulbabu <fulbabucse@gmail.com>
2022-10-24 21:15:23 +0600

    install firebase authentication

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbu86F9oRY0O4GONypltqHNzKRxH5Z60s",
+  authDomain: "brains-and-academics.firebaseapp.com",
+  projectId: "brains-and-academics",
+  storageBucket: "brains-and-academics.appspot.com",
+  messagingSenderId: "696181894906",
+  appId: "1:696181894906:web:0cf844617362a8d156e343",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
