454b0c84bf581e7529f6146536bf5a74ecef18ac
Fulbabu <fulbabucse@gmail.com>
2022-10-26 02:37:47 +0600

    add context provider

---
+++ src/contexts/UserProvider.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createContext } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+const AuthContext = createContext();
+const auth = getAuth(app);
+
+const UserProvider = ({ children }) => {
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
+
+export default UserProvider;
--- src/pages/UserActivities/Login/Login.jsx
+++ src/pages/UserActivities/Login/Login.jsx
@@ -1,9 +1,92 @@
 import React from "react";
+import { useState } from "react";
 
 const Login = () => {
+  const [error, setError] = useState("");
+  const handleUserSignIn = (e) => {
+    e.preventDefault();
+  };
+  const handleGoogleSignIn = () => {};
+  const handleFacebookSignIn = () => {};
   return (
-    <div>
-      <h1>This is Log in pages</h1>
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
+        <h1 className="text-3xl font-semibold text-center text-purple-700 uppercase">
+          Log In
+        </h1>
+        <p className="text-center text-purple-700">
+          Sign in to access your account
+        </p>
+        <form onSubmit={handleUserSignIn} className="mt-6">
+          <div className="mb-2">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              required
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              required
+            />
+          </div>
+          <span className="text-xs text-purple-600 hover:underline">
+            Forget Password?
+          </span>
+          <div className="mt-3">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+              Log In
+            </button>
+          </div>
+          <p className="text-red-400 mt-1">{error}</p>
+        </form>
+        <div className="relative flex items-center justify-center w-full mt-6 border border-t">
+          <div className="absolute px-5 bg-white">Or</div>
+        </div>
+        <div className="flex mt-4 gap-x-2">
+          <button
+            onClick={handleGoogleSignIn}
+            type="button"
+            className="flex items-center justify-center w-full p-2 border border-gray-600 rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600"
+          >
+            Continue with Google
+            <FaGoogle className="ml-2"></FaGoogle>
+          </button>
+          <button
+            onClick={handleFacebookSignIn}
+            className="flex items-center justify-center w-full p-2 border border-gray-600 rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600"
+          >
+            Continue with Facebook
+            <FaFacebook className="ml-2"></FaFacebook>
+          </button>
+        </div>
+
+        <p className="mt-8 text-xs font-light text-center text-gray-700">
+          Don't have an account?{" "}
+          <Link
+            to="/register"
+            className="font-medium text-purple-600 hover:underline"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
