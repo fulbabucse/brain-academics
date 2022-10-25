fb115cf1af17765099837d61db6a9b4c2bbecd48
Fulbabu <fulbabucse@gmail.com>
2022-10-25 23:15:02 +0600

    single course title add

--- src/pages/sharedPages/CourseDetails/CourseDetails.jsx
+++ src/pages/sharedPages/CourseDetails/CourseDetails.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import "../../../assets/style.css";
+import { FaStar, FaBookReader } from "react-icons/fa";
 
 const CourseDetails = () => {
-  const [openTab, setOpenTab] = React.useState(1);
+  //   const [openTab, setOpenTab] = React.useState(1);
   const courseDetailsData = useLoaderData()[0];
   const {
     id,
@@ -11,133 +12,70 @@ const CourseDetails = () => {
     author,
     course_content,
     course_name,
-    course_overview,
+    author_img,
     picture,
     published_date,
     student,
+    rating,
     title,
   } = courseDetailsData;
-  console.log(courseDetailsData);
+  console.log(course_content.html_css);
   return (
-    <div className="px-2 lg:px-20">
-      <div>
-        <h2></h2>
+    <div className="course-details-container px-2 lg:px-20 my-4">
+      <div className="text-slate-600">
+        <h2 className="text-4xl font-bold mb-3">{title}</h2>
+        <div className="flex gap-2">
+          <img
+            className="w-12 h-12 rounded-full"
+            src={author_img}
+            alt={author}
+          />
+          <div>
+            <h5 className="text-xl font-bold">{author}</h5>
+            <p>{published_date}</p>
+          </div>
+        </div>
+        <div className="flex gap-2 lg:w-1/4 justify-between">
+          <h4 className="font-semibold">Student: {student}</h4>
+          <span className="flex gap-1 items-center">
+            <FaStar className="text-pink-600"></FaStar>
+            <p>{rating}</p>
+          </span>
+        </div>
+
+        <div className="mt-6">
+          <h2 className="text-2xl font-bold text-slate-700 flex gap-2 items-center">
+            <span>
+              <FaBookReader></FaBookReader>
+            </span>
+            Course Content
+          </h2>
+          <div>
+            {course_content?.html_css?.map((content, idx) => (
+              <div key={idx}>
+                <div>
+                  <tbody>
+                    <tr className="lg:flex-none md:flex-none flex flex-col my-1">
+                      <td className="font-bold">{content.title}</td>
+                      <td className="text-md font-semibold text-slate-700">
+                        {content.desc}
+                      </td>
+                    </tr>
+                  </tbody>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
 
       {/* Tabs */}
       <div>
-        <div className="flex flex-wrap">
-          <div className="w-full">
-            <ul
-              className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
-              role="tablist"
-            >
-              <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-                <a
-                  className={
-                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-                    (openTab === 1
-                      ? "text-white bg-pink-400"
-                      : "text-black bg-white")
-                  }
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setOpenTab(1);
-                  }}
-                  data-toggle="tab"
-                  href="#link1"
-                  role="tablist"
-                >
-                  Profile
-                </a>
-              </li>
-              <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-                <a
-                  className={
-                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-                    (openTab === 2
-                      ? "text-white bg-pink-400"
-                      : "text-black bg-white")
-                  }
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setOpenTab(2);
-                  }}
-                  data-toggle="tab"
-                  href="#link2"
-                  role="tablist"
-                >
-                  Settings
-                </a>
-              </li>
-              <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-                <a
-                  className={
-                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-                    (openTab === 3
-                      ? "text-white bg-pink-400"
-                      : "text-black bg-white")
-                  }
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setOpenTab(3);
-                  }}
-                  data-toggle="tab"
-                  href="#link3"
-                  role="tablist"
-                >
-                  Options
-                </a>
-              </li>
-            </ul>
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
-              <div className="px-4 py-5 flex-auto">
-                <div className="tab-content tab-space">
-                  <div
-                    className={openTab === 1 ? "block" : "hidden"}
-                    id="link1"
-                  >
-                    <p>
-                      Collaboratively administrate empowered markets via
-                      plug-and-play networks. Dynamically procrastinate B2C
-                      users after installed base benefits.
-                      <br />
-                      <br /> Dramatically visualize customer directed
-                      convergence without revolutionary ROI.
-                    </p>
-                  </div>
-                  <div
-                    className={openTab === 2 ? "block" : "hidden"}
-                    id="link2"
-                  >
-                    <p>
-                      Completely synergize resource taxing relationships via
-                      premier niche markets. Professionally cultivate one-to-one
-                      customer service with robust ideas.
-                      <br />
-                      <br />
-                      Dynamically innovate resource-leveling customer service
-                      for state of the art customer service.
-                    </p>
-                  </div>
-                  <div
-                    className={openTab === 3 ? "block" : "hidden"}
-                    id="link3"
-                  >
-                    <p>
-                      Efficiently unleash cross-media information without
-                      cross-media value. Quickly maximize timely deliverables
-                      for real-time schemas.
-                      <br />
-                      <br /> Dramatically maintain clicks-and-mortar solutions
-                      without functional solutions.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis a
+          reprehenderit reiciendis accusantium voluptate explicabo totam
+          doloribus nam dolore repudiandae!
+        </h2>
       </div>
     </div>
   );
